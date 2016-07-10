b4d228e21428a0270cc7f505a8531bd76d9bc429
Caden <cadenkline9@gmail.com>
2016-07-10 13:06:22 -0400

    working encryption

---
+++ src/main/java/com/k_linedev/latin/encryption.java
@@ -0,0 +1,32 @@
+package com.k_linedev.latin;
+
+import java.util.ArrayList;
+
+/**
+ * Created by caden on 7/10/2016.
+ */
+public class encryption {
+    /**
+     * Encrypt to pig latin
+     *
+     * @param emessage
+     * @return lmessage + " "
+     */
+    public static String encrypt(String emessage) {
+        String ewords[] = emessage.split("\\s+");
+        ArrayList<String> lwords = new ArrayList<String>();
+        for (String eword : ewords) {
+            lwords.add(latin(eword.trim()));
+        }
+        String lmessage = "";
+        for (String lword : lwords) {
+            lmessage += lword + " ";
+        }
+        return lmessage;
+    }
+
+    private static String latin(String eword) {
+        return eword.substring(1) + eword.substring(0, 1) + "ay";
+
+    }
+}
--- src/test/java/latinTest.java
+++ src/test/java/latinTest.java
@@ -1,4 +1,5 @@
 import com.k_linedev.latin.decryption;
+import com.k_linedev.latin.encryption;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -13,4 +14,14 @@ public class latinTest {
         String lword = "ihay";
         assertEquals(eword, decryption.decrypt(lword));
     }
+
+    @Test
+    public void encryptionTest() {
+
+        String lword = "ihay ";
+        String eword = decryption.decrypt(lword);
+
+        assertEquals(lword, encryption.encrypt(eword));
+    }
+
 }
